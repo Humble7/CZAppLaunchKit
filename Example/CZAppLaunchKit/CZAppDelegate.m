690cc7a91d12a77d2de6e7977c3388cf1fc3fb37
ChenZhen <chenzhennba@gmail.com>
2022-09-25 23:50:58 +0800

    [applicationDidEnterBackground] applicationDidEnterBackground system phase.

--- Example/CZAppLaunchKit/CZAppDelegate.m
+++ Example/CZAppLaunchKit/CZAppDelegate.m
@@ -32,16 +32,20 @@
 {
     // Override point for customization after application launch.
     // 设置AnchorTask，当启动框架执行完AnchorTask后执行report trace的逻辑
-    [CZLKManager setAnchorTask:CZLK_ANCHOR_TASK_VIEW_DID_APPEAR_RECORD reportTrace:^(NSString * _Nonnull report, NSProgress * _Nonnull progress, NSDictionary * _Nonnull info) {
-        NSLog(@"[CZLK] [LOG] Report handler: \n %@", info);
-    }];
-
-    [CZLKManager setFinalTask:CZLK_ANCHOR_TASK_VIEW_DID_APPEAR_RECORD finalProgress:^(NSArray * _Nonnull lefts, NSProgress * _Nonnull progress) {
-        NSLog(@"[CZLK] [LOG] Finalize handler: \n %@ \n progress %@", lefts, progress);
-    }];
+//    [CZLKManager setAnchorTask:CZLK_ANCHOR_TASK_VIEW_DID_APPEAR_RECORD reportTrace:^(NSString * _Nonnull report, NSProgress * _Nonnull progress, NSDictionary * _Nonnull info) {
+//        NSLog(@"[CZLK] [LOG] Report handler: \n %@", info);
+//    }];
+//
+//    [CZLKManager setFinalTask:CZLK_ANCHOR_TASK_VIEW_DID_APPEAR_RECORD finalProgress:^(NSArray * _Nonnull lefts, NSProgress * _Nonnull progress) {
+//        NSLog(@"[CZLK] [LOG] Finalize handler: \n %@ \n progress %@", lefts, progress);
+//    }];
     CZLK_INVOKE_DID_FINISH_LAUNCH_START_TO_END();
     
     return YES;
+}
+
+-(void)applicationDidEnterBackground:(UIApplication *)application {
+    CZLK_INVOKE_VIEW_DID_ENTER_BACKGROUND_RECORD();
 }
 
 @end
